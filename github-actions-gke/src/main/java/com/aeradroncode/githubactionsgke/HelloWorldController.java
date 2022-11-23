00a524a2531e49e711590dbf53a0b9a9b36fa860
Akhilesh <106224697+akhileshgopal@users.noreply.github.com>
2022-11-23 10:43:14 +0530

    Add files via upload

---
+++ github-actions-gke/src/main/java/com/aeradroncode/githubactionsgke/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.aeradroncode.githubactionsgke;
+
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String helloWorld() { return "Hello World!"; }
+}
